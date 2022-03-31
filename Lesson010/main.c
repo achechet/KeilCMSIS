6953836461149f51616e7586ecd10d0062ca4128
achechet <achechet@gmail.com>
2022-03-31 09:31:45 +0300

    Lesson010

---
+++ Lesson010/main.c
@@ -0,0 +1,37 @@
+#include "stm32f10x.h"                  // Device header
+#include "ds18b20.h"
+#include "tim6.h"
+
+/* USER CODE BEGIN PV */
+#define PORT  GPIOC   //указать порт, к которому подключены датчики
+#define PIN 7
+
+		uint8_t ds_buff[9];
+		uint8_t i,temp,b;
+		uint16_t raw_temper;
+		float temper;
+
+int main(void) 
+	{
+		TIM6_Config();
+		ds18b20_PortInit();
+
+		while(1) 
+			{
+				ds18b20_Reset();
+				ds18b20_SendByte(0xCC);
+				ds18b20_SendByte(0x44);
+				Delay_us(1000);
+				ds18b20_Reset();
+				ds18b20_SendByte(0xCC);	
+				ds18b20_SendByte(0xBE);		
+				for(i=0; i<9; i++ )           //прочитать 9 байт в массив
+				ds_buff[i] = ds18b20_ReadByte();
+//				temp = ds_buff[1];
+//				temp =temp <<8;
+//				temp |= ds_buff[0];
+				raw_temper = ((uint16_t) ds_buff[1]<<8)|ds_buff[0];
+				temper = ds18b20_Convert(raw_temper);
+				Delay_ms(10);
+			}
+	}
